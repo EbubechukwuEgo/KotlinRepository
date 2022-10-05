c00e70c07746fc37babc718a5bbaf4b7cece1fc7
Ebubechukwu Ego-Osuala <ebube17higa@gmail.com>
2022-10-05 19:41:03 +0100

    I'll be King of the Github Commits

--- src/main/kotlin/classwork/CharactersInAscii.kt
+++ src/main/kotlin/classwork/CharactersInAscii.kt
@@ -1,5 +1,14 @@
 package classwork
 
+import java.util.Scanner
+
 fun main(){
 
+    val dragon = Scanner(System.`in`)
+
+    print("Enter character here: ")
+    val sabo = dragon.next().single()
+
+    val ascii = sabo.code
+    println("The ascii value for the character $sabo is $ascii")
 }
