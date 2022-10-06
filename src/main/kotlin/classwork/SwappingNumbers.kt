7de297c2dcc07b0738d61066576bcc3e84d48e69
Ebubechukwu Ego-Osuala <ebube17higa@gmail.com>
2022-10-06 09:52:14 +0100

    I'm going to commit this file to the repository, and push it.

---
+++ src/main/kotlin/classwork/SwappingNumbers.kt
@@ -0,0 +1,12 @@
+package classwork
+
+fun main(){
+
+    var firstNumber = 3
+
+    var secondNumber = firstNumber
+
+    firstNumber = 8
+
+    println("First number: $firstNumber, Second number: $secondNumber")
+}
