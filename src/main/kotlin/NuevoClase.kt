61305eefd5074749a16e4716adabc8ed2ec45daf
Ebubechukwu Ego-Osuala <ebube17higa@gmail.com>
2022-10-13 10:10:17 +0100

    You're Gonna Carry that Weight.

--- src/main/kotlin/NuevoClase.kt
+++ src/main/kotlin/NuevoClase.kt
@@ -125,4 +125,34 @@ nested if statements
 //        }
 //        num1--
 //    }
+
+    // continue
+//    for (i in 1..5){
+//        println("$i Always printed.")
+//        if (i > 1 && i < 5){
+//            continue
+//        }
+//        println("$i Not always printed")
+//    }
+
+//    var number : Int
+//    var sum = 0
+//    for (i in 1..6){
+//        print("Enter an integer: ")
+//        number = readLine()!!.toInt()
+//        if (number <= 0)
+//            continue
+//        sum += number
+//    }
+//    println("sum = $sum")
+
+    // label continues
+//    here@ for (i in 1..5){
+//        for (j in 1..4){
+//            if (i ==3 || j == 2)
+//                continue@here
+//            println("i = $i; j = $j")
+//        }
+//    }
+
 }
